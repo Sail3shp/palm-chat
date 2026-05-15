ba608e04ebe7d48a080a3e78682a1ac937b71519
Sail3shp <saileshpokhrel557@gmail.com>
2026-05-15 22:16:43 +0545

    chore(mongodb): add mongoose connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,19 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import { connectDb } from './config/connectDb.js'
+
+dotenv.config()
 
 const app = express()
+console.log(process.env.PORT)
+const PORT = process.env.PORT || 8848
 
 app.get('/health',(req,res) => {
     res.send("All good")
 })
 
 
-app.listen(5432,() => {
-    console.log('server is running ')
+app.listen(PORT,async() => {
+    await connectDb()
+    console.log(`server is running on port ${PORT}`)
 })
